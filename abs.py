598e90065765161d9ba790a1991a634c92ae3434
selimcanpolat <76406395+selimcanpolat@users.noreply.github.com>
2021-01-22 14:48:08 +0300

    Add files via upload

---
+++ abs.py
@@ -0,0 +1,9 @@
+# Write a program which takes a float and prints the absolute value of this float without using abs() function.
+
+while True:
+
+    num = float(input("please enter a number.\n"))
+
+    import math
+
+    print(abs(num))
