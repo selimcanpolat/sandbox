ae476f313562488b14de394d70389c5d85488df7
selimcanpolat <76406395+selimcanpolat@users.noreply.github.com>
2021-07-13 13:39:29 +0300

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,3 +1,5 @@
+# returns a key given a sorted array with unique elements 
+
 def binary_search(sequence,item):
 
     lower_index = 0
@@ -9,17 +11,14 @@ def binary_search(sequence,item):
         first_half = sequence[0:half]
         second_half = sequence[half:len(sequence)]
 
-        if item in first_half:
+        if item <= first_half[len(first_half)-1]:
             sequence = first_half
             upper_index -= half
 
-        elif item in second_half:
+        elif item >= second_half[len(second_half)-1]:
             sequence = second_half
             lower_index += half
         else:
             return None
 
     return lower_index
-
-
-
